ec346f390eb11a3a8c5bf43ba73cbc8326119066
Witty Chang <wcc1213@gmail.com>
2017-08-06 23:21:08 -0700

    Edit js files.

--- js/findhelp.js
+++ js/findhelp.js
@@ -4,14 +4,13 @@ function FindHelp() {
 }
 
 FindHelp.prototype.getDoctors = function(symptom, getDoctorList) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+booboo+'&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key='+apiKey).then(function(response) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ booboo + '&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key='+ apiKey).then(function(response) {
     response.data.forEach(function(listing) {
       getDoctorList(listing);
       console.log(JSON.stringify.response);
   });
   }).fail(function(error) {
     console.log("Search failed");
-    $('#fail').text(error.responseJSON.message);
   });
 };
 
